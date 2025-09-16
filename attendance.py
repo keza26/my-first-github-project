79113217e8bc3f3e8515b4958ac4289abda9f472
keza26 <kezaglo2021@gmail.com>
2025-09-16 23:52:12 +0200

    describe the changes

--- attendance.py
+++ attendance.py
@@ -1,1 +1,14 @@
-print("hello,this is my first Python file connected to Github!")
+
+import requests
+from datetime import date
+url = "https://XYZ.erpnext.com/api/resource/attendance"
+
+headers ={
+    "Authorization":"token b8117b2c836c634:***************"
+}
+data ={
+    "employee":"HR-EMP-00001",
+    "status":"Present"
+}
+response =requests.post(url,headers=headers,json=data)
+print(response.json())
